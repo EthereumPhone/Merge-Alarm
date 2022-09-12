541e65ae709377a7b8db83e870c202168ad5c8f5
Markus Haas <haas.markus@protonmail.com>
2022-09-12 11:08:16 +0200

    first commit

---
+++ app/src/main/java/org/ethereumphone/mergealarm/AlarmReceiver.java
@@ -0,0 +1,20 @@
+package org.ethereumphone.mergealarm;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.widget.Toast;
+
+public class AlarmReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        System.out.println("Alarm has been called");
+        Toast.makeText(context, "Alarm!", Toast.LENGTH_LONG).show();
+        Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+        Ringtone r = RingtoneManager.getRingtone(context, notification);
+        r.play();
+    }
+}
