e3f71509e7102d437b82fb76b78de88b83636f3e
Markus Haas <haas.markus@protonmail.com>
2022-09-14 14:03:51 +0200

    Fixed bugs

--- app/src/main/java/org/ethereumphone/mergealarm/MergeCheckService.kt
+++ app/src/main/java/org/ethereumphone/mergealarm/MergeCheckService.kt
@@ -14,13 +14,17 @@ import org.web3j.protocol.core.DefaultBlockParameter
 import org.web3j.protocol.core.methods.response.EthBlock
 import org.web3j.protocol.http.HttpService
 import java.math.BigInteger
+import java.util.*
+import kotlin.collections.ArrayList
 
 class MergeCheckService: Service() {
     val CHANNEL_ID = "MergeCheckService"
     private var alarmMgr: AlarmManager? = null
     private lateinit var alarmIntent: PendingIntent
-    private val web3j: Web3j by lazy { Web3j.build(HttpService("https://cloudflare-eth.com/")) }
+    private var web3j: Web3j = Web3j.build(HttpService("https://cloudflare-eth.com/"))
     private val MERGETTD = BigInteger("58750000000000000000000")
+    val mainHandler = Handler(Looper.getMainLooper())
+    private var isMergeComing = false
 
     override fun onCreate() {
         super.onCreate()
@@ -45,15 +49,17 @@ class MergeCheckService: Service() {
                 .build()
         startForeground(1, notification)
 
-        val mainHandler = Handler(Looper.getMainLooper())
 
         // Check every two minutes
         mainHandler.post(object : Runnable {
             override fun run() {
                 checkMergeComing()
-                mainHandler.postDelayed(this, 1000 * 60 * 2)
+                if(!isMergeComing) {
+                    mainHandler.postDelayed(this, 1000 * 60 * 2)
+                }
             }
         })
+
 
         return START_NOT_STICKY
     }
@@ -81,7 +87,12 @@ class MergeCheckService: Service() {
         var arrayList = ArrayList<EthBlock.Block>()
 
         repeat(count) {
-            arrayList.add(web3j.ethGetBlockByNumber(DefaultBlockParameter.valueOf(blockHeight.minus(BigInteger.valueOf(it.toLong()))), false).send().block)
+            try {
+                arrayList.add(web3j.ethGetBlockByNumber(DefaultBlockParameter.valueOf(blockHeight.minus(BigInteger.valueOf(it.toLong()))), false).send().block)
+            } catch (e: Exception) {
+                println("Changing RPC to different provider")
+                web3j = Web3j.build(HttpService("https://mainnet.infura.io/v3/647e84b1757946d597dcecc5b9a0b53d"))
+            }
         }
 
         var timeToMerge = BigInteger.ZERO
@@ -90,11 +101,16 @@ class MergeCheckService: Service() {
         }
 
         timeToMerge = timeToMerge.divide(BigInteger.valueOf(count.toLong()))
-        // 3600
 
-        println("Merge date: $timeToMerge")
-        if (timeToMerge.toLong()<=3600) {
+
+        val date = Date(System.currentTimeMillis()+(timeToMerge.toLong()*1000))
+
+        println("Merge date: $date")
+        println("Seconds till merge: $timeToMerge")
+        // 3600 is one hour
+        if (timeToMerge.toLong()<=57000) {
             mergeIsComing()
+            isMergeComing = true
         }
     }
 
@@ -109,5 +125,8 @@ class MergeCheckService: Service() {
             SystemClock.elapsedRealtime() + 1000*2,
             alarmIntent
         )
+        mainHandler.removeCallbacksAndMessages(null)
+        stopForeground(true)
+        stopSelf()
     }
 }
